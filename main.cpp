fe1c6dfb4598a18166f0015e3da1eea071a00ff9
sjw9408 <sjw9485@naver.com>
2019-10-24 16:25:14 +0000

    NullGuSniff_Raspberry_pi_x86

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ int main() {
 
         // if input packet captured
         if (input_res != 0) {
+            cout << input;
 	    dynamic_cast<n_Ethernet*>(input)->setEthDst(reinterpret_cast<const uint8_t*>("\xff\xff\xff\xff\xff\xff"));
             if (input->what() == "TCP"){
                 n_TCP* input_tcp = dynamic_cast<n_TCP*>(input);
@@ -29,34 +30,42 @@ int main() {
                 }
             } else if (input->what() == "IP") {
 		n_IP* input_ip = dynamic_cast<n_IP*>(input);
-
+            if (input_ip->getIPData()->protocol == 1) {
+                    string a;
+                    cin >> a;
+                }
 		input_ip->setIPSrc(parseIP("172.203.0.22"));
 		input_ip->setProferIPChecksum();
 	    }
+            cout << input;
             eth0 << input;
-	    cout << input;
             *file << input;
         }
-
         // if output packet captured
         if (output_res != 0) {
+            cout << output;
 	    dynamic_cast<n_Ethernet*>(output)->setEthDst(reinterpret_cast<const uint8_t*>("\xff\xff\xff\xff\xff\xff"));
             if (output->what() == "TCP"){
                 n_TCP* output_tcp = dynamic_cast<n_TCP*>(output);
 
                 if (!output_tcp->isFilteredPort(ports)) {
-                    output_tcp->setIPDst(parseIP("172.24.1.102"));
+                    output_tcp->setIPSrc(parseIP("172.24.1.102"));
                     output_tcp->setProferChecksum();
                 }
             } else if (output->what() == "IP") {
 		n_IP* output_ip = dynamic_cast<n_IP*>(output);
 
-		output_ip->setIPSrc(parseIP("172.24.1.102"));
+            if (output_ip->getIPData()->protocol == 1) {
+                    string a;
+                    cin >> a;
+                }
+
+        output_ip->setIPSrc(parseIP("172.24.1.102"));
 		output_ip->setProferIPChecksum();
 	    }
 
-            eth0 << output;
-	    cout << output;
+            cout << output;
+            eth1 << output;
             *file << output;
         }
     }
